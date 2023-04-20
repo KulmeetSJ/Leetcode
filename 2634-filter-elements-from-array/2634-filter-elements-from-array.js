c6992ac5f6b21d202e2c290b699bf89bbdadfc01
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-04-20 13:59:13 +0530

    Time: 66 ms (15.03%), Space: 42.1 MB (51.22%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var newArr = [];
+    for(let i=0;i<arr.length;i++)
+    {
+        element = arr[i];
+        if(fn(element,i))
+            newArr.push(element);
+    }
+    return newArr;
+};
