35268fb2304157e022c1c405a31223fba3dc038d
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-05-27 13:11:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,43 +1,16 @@
 class Solution {
 public:
-    //Recursion
-    int solve(int i,int j, int m,int n)
-    {
-        if(i >= m || j>= n)
-        {   
-            return 0;
-        }
-        if(i == m-1 && j== n-1)
-            return 1;
-        else 
-            return solve(i+1,j,m,n) + solve(i,j+1,m,n);        
-    }
-    
-    //Recursion + Memoization
-    int solveMem(int i,int j,int m,int n,vector<vector<int>> &dp)
-    {
-        if(i >=m || j>=n)
-        {
-            return 0;
-        }
-        if(i == m-1 && j == n-1)
-            return 1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        return dp[i][j] = solveMem(i+1,j,m,n,dp)+ solveMem(i,j+1,m,n,dp);
-        
-    }
-    
-    
-    
     int uniquePaths(int m, int n) {
-        //Recursion
-        //return solve(0,0,m,n);
+        int N = m + n - 2;
+        int R = m-1;
         
-        vector<vector<int>>dp(m,vector<int>(n,-1));
+        double ans = 1;
         
-        return solveMem(0,0,m,n,dp);
+        for(int i=1;i<=R;i++)
+        {
+            ans = ans * (N-R+i);
+            ans = ans / i;
+        }
+    return (int)ans;
     }
 };
