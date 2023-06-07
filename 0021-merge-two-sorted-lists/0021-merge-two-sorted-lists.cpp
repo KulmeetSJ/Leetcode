250a698d484c2cbb6abd9045f27bbab70d751da7
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-06-07 13:47:18 +0530

    Time: 4 ms (92.98%), Space: 14.8 MB (67.05%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,45 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2 == NULL) return NULL;
-        else if(list1 == NULL && list2 != NULL) return list2;
-        else if(list1 != NULL && list2 == NULL) return list1;
+    // void swap(ListNode* l1,ListNode* l2)
+    // {
+    //     ListNode* temp = l1;
+    //     l1 = l2;
+    //     l2 = temp;
+    // }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         
-        ListNode* p = list1 , *q = list2;
-        ListNode* newHead = new ListNode(-1);
-        ListNode* k = newHead;
-        while(p!= NULL && q!= NULL)
-        {   
-            ListNode* temp = new ListNode();
-            if(p->val <= q->val)
+        
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        if(l1->val > l2->val){
+            std::swap(l1,l2);
+        }
+        
+        ListNode* res = l1;
+        
+        while(l1!=NULL && l2!=NULL)
+        {
+            ListNode* temp = NULL;
+            while(l1 != NULL && l1->val <= l2->val)
             {
-                temp->val = p->val;
-                p = p->next;
+                temp = l1;
+                l1 = l1->next;
             }
-            else{
-                temp->val = q->val;
-                q=q->next;
-            }
-            k->next = temp;
-            k = k->next;
+            temp->next = l2;
+            std::swap(l1,l2);
         }
-        
-        while(p!=NULL)
-        {
-            ListNode* temp = new ListNode(p->val);
-            p = p->next;
-            k->next = temp;
-            k = k->next;
-        }
-        while(q!=NULL)
-        {
-            ListNode* temp = new ListNode(q->val);
-            q = q->next;
-            k->next = temp;
-            k = k->next;
-        }
-        
-    return newHead->next;
+        return res;
     }
 };
