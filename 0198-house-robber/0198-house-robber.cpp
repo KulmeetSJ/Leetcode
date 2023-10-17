c744b6c1d052fac62a8c20f50dd82b4db4fc60b5
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-17 22:51:25 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (10.53%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,8 +11,27 @@ public:
         
         return dp[ind] = max(pick,notPick);
     }
+    int tabu(vector<int> nums){
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        dp[0] = nums[0];
+        
+        
+        for(int ind = 1;ind<n;ind++){
+            
+            int pick = nums[ind];
+            if(ind > 1 ){
+                pick +=  dp[ind-2];
+            }
+            
+            int notPick = dp[ind-1];
+            dp[ind] = max(pick,notPick);
+        }
+    return dp[n-1];
+    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return rec_mem(nums.size()-1,nums,dp);
+        //vector<int> dp(nums.size(),-1);
+        //return rec_mem(nums.size()-1,nums,dp);
+        return tabu(nums);
     }
 };
