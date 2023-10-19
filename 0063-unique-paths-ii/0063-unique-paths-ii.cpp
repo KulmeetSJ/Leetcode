b34b6b6fda91090c90cd268758adb7d6ee93d552
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-19 20:24:59 +0530

    Time: 2 ms (60.82%), Space: 8.1 MB (22.74%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-    int solve(int i,int j,int m,int n,vector<vector<int>>& obstacleGrid)
-    {
-        if(i>=m || j>=n || obstacleGrid[i][j] == 1)
-            return 0;
-        if(i==m-1 && j== n-1)
-            return 1;
+    int rec_mem(int i,int j,vector<vector<int>> &a,vector<vector<int>>& dp){
+        if(i>=0 && j>=0 && a[i][j] == 1) return 0;
+        if(i == 0 && j == 0) return 1;
+        if(i<0 || j<0 )return 0;
         
-        return solve(i+1,j,m,n,obstacleGrid) + solve(i,j+1,m,n,obstacleGrid);
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int up = rec_mem(i-1,j,a,dp);
+        int left = rec_mem(i,j-1,a,dp);
+        
+        return dp[i][j] = up+left;
     }
-    
-    int solveMem(int i,int j,int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp)
-    {
-        if(i>=m || j>=n || obstacleGrid[i][j] == 1)
-            return 0;
-        if(i == m-1 && j==n-1)
-            return 1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        return dp[i][j] = solveMem(i+1,j,m,n,obstacleGrid,dp) + solveMem(i,j+1,m,n,obstacleGrid,dp);
-    }
-    
-    
-    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        
-        //Recursion ---> TLE
-        //return solve(0,0,m,n,obstacleGrid);
-        
-        //Recursion + Memoization
-        
+        int m = obstacleGrid.size(),n = obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        
-        return solveMem(0,0,m,n,obstacleGrid,dp);
+        return rec_mem(m-1,n-1,obstacleGrid,dp);
     }
 };
