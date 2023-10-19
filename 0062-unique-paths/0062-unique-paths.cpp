0b55a1009ab604cc239a59268a522b675073ab53
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-19 18:43:05 +0530

    Time: 3 ms (27.91%), Space: 6.4 MB (61.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -30,10 +30,31 @@ public:
         }
     return dp[m-1][n-1];
     }
+    
+    int space_opt(int m,int n){
+        vector<int> prev(n,0);
+        
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0) curr[j] = 1;
+                else{
+                    int up = 0 , left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up + left;
+                }    
+            }
+            prev = curr;
+        }
+    return prev[n-1];
+    }
     int uniquePaths(int m, int n) {
         //vector<vector<int>> dp(m,vector<int>(n,-1));
         //return rec_mem(m-1,n-1,dp);
         
-        return tabu(m,n);
+        //return tabu(m,n);
+        
+        return space_opt(m,n);
     }
 };
