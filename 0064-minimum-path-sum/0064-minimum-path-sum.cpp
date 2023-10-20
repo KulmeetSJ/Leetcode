9235dad73320abf59da02b5c2ae1e6e44fcb31a9
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-20 12:43:55 +0530

    Time: 11 ms (23.71%), Space: 10.5 MB (8.40%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -10,9 +10,29 @@ public:
         int left = grid[i][j] + memo(i,j-1,grid,dp);
         return dp[i][j] = min(up,left);
     }
+    int tabu(vector<vector<int>>& grid,int m,int n){
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0) dp[i][j] = grid[i][j];
+                else{ 
+                    int up = grid[i][j]; int left = grid[i][j];
+                    if( i> 0 ) up += dp[i-1][j];
+                    else up += 1e9;
+                    if (j > 0) left += dp[i][j-1];
+                    else left += 1e9;
+                    
+                    dp[i][j] = min(up,left);
+                }
+            }
+        }
+    return dp[m-1][n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size() , n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return memo(m-1,n-1,grid,dp);
+        //vector<vector<int>> dp(m,vector<int>(n,-1));
+        //return memo(m-1,n-1,grid,dp);
+        
+        return tabu(grid,m,n);
     }
 };
