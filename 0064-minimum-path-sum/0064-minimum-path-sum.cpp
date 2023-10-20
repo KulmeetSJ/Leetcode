9a06766d79c63a0b68a79d4ce3a04057314db6f2
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-20 12:49:17 +0530

    Time: 4 ms (84.78%), Space: 10.2 MB (40.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -28,11 +28,34 @@ public:
         }
     return dp[m-1][n-1];
     }
+    
+    int space_opt(vector<vector<int>>& grid,int m,int n){
+        vector<int> prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if( i == 0 && j == 0) curr[j] = grid[i][j];
+                else{
+                    int up = grid[i][j]; int left = grid[i][j];
+                    if(i>0) up += prev[j];
+                    else up += 1e9;
+                    if(j>0) left += curr[j-1];
+                    else left += 1e9;
+                    
+                    curr[j] = min(up,left);
+                }
+            }
+            prev = curr;
+        }
+    return prev[n-1];
+    }
+    
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size() , n = grid[0].size();
         //vector<vector<int>> dp(m,vector<int>(n,-1));
         //return memo(m-1,n-1,grid,dp);
         
-        return tabu(grid,m,n);
+        //return tabu(grid,m,n);
+        return space_opt(grid,m,n);
     }
 };
