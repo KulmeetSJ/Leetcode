03b3ebcfb2380eab296f1bf300e6715307d5cbc2
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-20 15:34:22 +0530

    Time: 0 ms (100.00%), Space: 12.3 MB (49.24%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
         int break_point = -1;
-        for(int i=n-2;i>=0;i--)
-        {
-            if(nums[i]<nums[i+1])
-            {
+        int n = nums.size();
+        
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
                 break_point = i;
                 break;
             }
         }
-        if(break_point == -1)
-        {
+        if(break_point == -1){
             reverse(nums.begin(),nums.end());
             return;
         }
-        else
-        {   
-            for(int i=n-1;i>=break_point;i--)
-            {
-                if(nums[i]>nums[break_point])
-                {
-                    swap(nums[i],nums[break_point]);
-                    break;
+        
+        int mini = INT_MAX;
+        int mini_index = break_point;
+        
+        for(int i=n-1;i>=break_point;i--){
+            if(nums[i] > nums[break_point]){
+                if(mini > nums[i]){
+                    mini = nums[i];
+                    mini_index = i;
                 }
             }
-            reverse(nums.begin()+break_point+1,nums.end());
-            return;
         }
+        
+        swap(nums[mini_index],nums[break_point]);
+        
+        reverse(nums.begin()+break_point+1,nums.end());
     }
 };
