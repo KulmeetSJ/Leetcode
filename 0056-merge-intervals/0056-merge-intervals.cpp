c283b32c0ff4ec0570633cedbafe7654c677f621
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-20 18:12:53 +0530

    Time: 30 ms (62.81%), Space: 19.4 MB (55.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,20 +6,12 @@ public:
         sort(intervals.begin(),intervals.end());
         
         for(int i=0;i<intervals.size();i++){
-            int start = intervals[i][0] , end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-            for(int j=i+1;j<intervals.size();j++){
-                
-                if(intervals[j][0] <= end){
-                    end = max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
     }
