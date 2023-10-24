d49f7e7314d8bb0b5b689c5e152bebd78530e9d1
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-24 13:59:38 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (36.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,25 +3,18 @@ public:
     double myPow(double x, int n) {
         double ans = 1.0;
         long long nn = n;
-        if(nn<0)
-            nn*=-1;
-        
-        while(nn)
-        {
-            if(nn%2 == 0)
-            {
+        if(n < 0) nn = -1 * nn;
+        while(nn){
+            if(nn%2 == 0){
                 x = x*x;
                 nn/=2;
             }
-            else
-            {
+            else{
                 ans = ans*x;
-                nn-=1;
+                nn -=1;
             }
         }
-        
-        if(n<0) ans = (double)(1.0) / (double)(ans);
-        
+        if(n<0) ans = (double)(1.0) / (double)ans;
         return ans;
     }
 };
