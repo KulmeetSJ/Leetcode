938bc68a4e1cf855862468ab0d8fae501e98abbc
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-10-29 21:55:31 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (80.46%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    void solve(int index,vector<int> &ds,vector<int> &nums,vector<vector<int>> &ans){
-        
+    void solve(int idx,vector<int>& nums,vector<int>& ds,vector<vector<int>> &ans){
+       
         ans.push_back(ds);
-        
-        for(int i=index;i<nums.size();i++){
-            if(i!=index && nums[i] == nums[i-1]) continue;
-            
+        for(int i=idx;i<nums.size();i++){
+            if(i>idx && nums[i] == nums[i-1]) continue;
             ds.push_back(nums[i]);
-            solve(i+1,ds,nums,ans);
+            solve(i+1,nums,ds,ans);
             ds.pop_back();
         }
     }
@@ -18,7 +16,8 @@ public:
         
         sort(nums.begin(),nums.end());
         
-        solve(0,ds,nums,ans);
+        solve(0,nums,ds,ans);
+        
         
     return ans;
     }
