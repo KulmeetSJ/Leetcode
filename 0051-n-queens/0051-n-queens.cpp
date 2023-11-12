e0551b5506c5b649356fb463a229396cc83815c0
Kulmeet Singh <singhkulmeet3@gmail.com>
2023-11-12 12:33:19 +0530

    Time: 3 ms (87.19%), Space: 7.6 MB (64.97%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> &board,int n){
-        //Checking left for Queen
+    bool isSafe(int row,int col,vector<string>& board,int n){
         for(int j=col;j>=0;j--){
-            if(board[row][j] == 'Q')  return false;
+            if(board[row][j] == 'Q') return false;
         }
         
-        //Left Upper diagonal
+        
         for(int i=row,j=col;i>=0 && j>=0;i--,j--){
             if(board[i][j] == 'Q') return false;
         }
         
-        //Left Lower Diagonal
+        
         for(int i=row,j=col;i<n && j>=0;i++,j--){
             if(board[i][j] == 'Q') return false;
         }
-return true;
-}
-    
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+    return true;
+    }
+    void solve(int col,int n,vector<vector<string>> &ans,vector<string> &board){
+        
         if(col == n){
             ans.push_back(board);
             return;
         }
         
-        for(int row=0;row<n;row++){
+        for(int row = 0;row<n;row++){
             if(isSafe(row,col,board,n)){
                 board[row][col] = 'Q';
-                solve(col+1,board,ans,n);
+                solve(col+1,n,ans,board);
                 board[row][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
         string str(n,'.');
         vector<string> board(n,str);
-        vector<vector<string>> ans;
         
-        solve(0,board,ans,n);
+        solve(0,n,ans,board);
+        
     return ans;
     }
 };
